318ce594d5c43f2a5f725ec1e24a8073b33f0065
VonBorstel-J <jordanvonborstel@gmail.com>
2022-08-09 16:34:06 -0700

    dasf

--- m4-w3-d1-demo/src/components/MyComponent.test.js
+++ m4-w3-d1-demo/src/components/MyComponent.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import MyComponent from './MyComponent';
+import { it } from 'node:test';
 
 describe("MyComponent", () => {
   it("should render correctly", () => {
+    const wrapper = shallow(<MyComponent />);
+  });
+  it("should render initial layout", () => { 
+    //when
     const component = shallow(<MyComponent />);
+    //then
+    expect(component.getElements()).toMatchSnapshot();
   });
 });
